160410ed45b1b609854cd02fa87f300e443c8a5b
limgu <lim9@tars.pusan.ac.kr>
2025-07-14 19:22:19 +0900

    2nd commit of lgpyred pipeline with full directory structure

--- setup.py
+++ setup.py
@@ -3,19 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='lgpyred',
     version='0.1',
-    description='LGPY Image Reduction Pipeline',
+    description='LGPY REDuction pipeline',
     author='Gu Lim',
     packages=find_packages(),
     include_package_data=True,
-    package_data={
-        'lgpyred.lgpytars': [
-            'data/*/*.*',
-            'astrom_config/*.*',
-            'photconf/*.*',
-            'sex_config/*.*',
-            'reduction/*.*',
-        ],
-    },
     install_requires=[
         'numpy',
         'astropy',

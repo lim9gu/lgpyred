e4b765115ec9fa16bf34d56c5d27a5f59cd64113
limgu <lim9@tars.pusan.ac.kr>
2025-07-14 19:11:18 +0900

    2nd commit of lgpyred pipeline with full directory structure

--- setup.py
+++ setup.py
@@ -18,4 +18,24 @@ setup(
         ]
     },
     include_package_data=True,
+    package_data={
+        'lgpyred.lgpytars': [
+            'data/**/*.*',
+            'astrom_config/*.*',
+            'photconf/*.*',
+            'sex_config/*.*',
+            'reduction/*.*',
+        ],
+    },
+    install_requires=[
+        'numpy',
+        'astropy',
+        'pyraf',
+        'paramiko',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lgpyred = lgpyred.lgpyred:main'
+        ]
+    },
 )

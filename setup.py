308dd57915baa91da678083841b3fc74f71123a4
limgu <lim9@tars.pusan.ac.kr>
2025-07-15 12:08:05 +0900

    commit of lgpyred pipeline with full directory structure

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ setup(
     author='Gu Lim',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'numpy',
-        'astropy',
-        'pyraf',
-        'paramiko',
-    ],
+	install_requires=[
+		'numpy==1.26.4',
+		'astropy==6.0.1',
+		'pyraf==2.2.2',
+		'paramiko==3.4.0',
+		'matplotlib==3.8.4',
+		'scipy==1.12.0',
+		'astroquery==0.4.7',
+		'pillow==9.2.0',
+	],
     entry_points={
         'console_scripts': [
             'lgpyred = lgpyred.lgpyred:main'
